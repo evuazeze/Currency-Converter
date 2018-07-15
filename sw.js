66940c390ce41ac83b2b1eb2f66febb7a8568157
zeze <evuazeze.emmanuel@gmail.com>
2018-07-15 09:04:20 +0100

    sw updated

--- sw.js
+++ sw.js
@@ -19,12 +19,7 @@ self.addEventListener('install', function(event) {
         'images/icons/icon-48x48.png',
         'images/icons/icon-96x96.png',
         'images/icons/icon-128x128.png',
-        'images/icons/icon-144x144.png',
-        'images/icons/icon-152x152.png',
-        'images/icons/icon-192x192.png',
-        'images/icons/icon-256x256.png',
-        'images/icons/icon-384x384.png',
-        'images/icons/icon-512x512.png'
+        'images/icons/icon-144x144.png'
       ]);
     })
   );
