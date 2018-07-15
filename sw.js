a2dbc510ebda1f7a81064dd8c1a4c9c348aaae67
zeze <evuazeze.emmanuel@gmail.com>
2018-07-15 14:22:45 +0100

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var staticCacheName = 'currency-static-v67';
+var staticCacheName = 'currency-static-v68';
 var allCaches = [
   staticCacheName];
 
@@ -10,10 +10,10 @@ self.addEventListener('install', function(event) {
         './scripts/main.js',
         './scripts/views/View.js',
         './scripts/models/Model.js',
-        './scripts/Controller.js'
-        // 'scripts/idb/CurrencyIDB.js',
-        // 'styles/styles.css',
-        // 'https://free.currencyconverterapi.com/api/v5/currencies',
+        './scripts/Controller.js',
+        './scripts/idb/CurrencyIDB.js',
+        './styles/styles.css',
+        'https://free.currencyconverterapi.com/api/v5/currencies',
         // 'images/icons/icon-48x48.png',
         // 'images/icons/icon-96x96.png',
         // 'images/icons/icon-128x128.png',
